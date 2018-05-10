9bd1845b035f36e96910898eaa03e33745310a21
Ellen Shapiro <designatednerd@gmail.com>
2018-05-10 11:34:44 +0200

    pre-dvorak kotlin example

---
+++ kotlin/src/EnumTricks.kt
@@ -0,0 +1,55 @@
+
+enum class KeyboardType {
+  QWERTY,
+  ABCDEF,
+  QWERTZ,
+  AZERTY;
+
+  fun next(): KeyboardType {
+    val allKeyboards = KeyboardType.values()
+    val currentIndex = allKeyboards.indexOf(this)
+    val nextIndex = currentIndex + 1
+    return if (nextIndex >= allKeyboards.size) {
+      allKeyboards[0]
+    }  else {
+      allKeyboards[nextIndex]
+    }
+  }
+
+  fun keys(): List<List<String>> {
+    return when (this) {
+      QWERTY -> listOf(
+          listOf("Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"),
+          listOf("A", "S", "D", "F", "G", "H", "J", "K", "L"),
+          listOf("Z", "X", "C", "V", "B", "N", "M")
+      )
+      ABCDEF -> listOf(
+          listOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"),
+          listOf("K", "L", "M", "N", "O", "P", "Q", "R", "S"),
+          listOf("T", "U", "V", "W", "X", "Y", "Z")
+      )
+      QWERTZ -> listOf(
+          listOf("Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P"),
+          listOf("A", "S", "D", "F", "G", "H", "J", "K", "L"),
+          listOf("Y", "X", "C", "V", "B", "N", "M")
+      )
+      AZERTY -> listOf(
+          listOf("A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P"),
+          listOf("Q", "S", "D", "F", "G", "H", "J", "K", "L", "M"),
+          listOf("W", "X", "C", "V", "B", "N")
+      )
+    }
+  }
+}
+
+fun main(args: Array<String>) {
+
+  for (keyboard in KeyboardType.values()) {
+    println("${keyboard.name} is at index ${keyboard.ordinal}")
+  }
+
+  val first = KeyboardType.AZERTY
+
+  val next = first.next()
+  println("After ${first.name} is ${next.name}")
+}
