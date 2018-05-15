5bcc31a43674967a64002eaeb701e002ce8b2c32
Ellen Shapiro <designatednerd@gmail.com>
2018-05-16 00:26:00 +0200

    clearly what this needs is more cats

---
+++ swift/StupidEnumTricks/StupidEnumTricks/Enums/ImageAsset.swift
@@ -0,0 +1,34 @@
+//
+//  ImageAsset.swift
+//  StupidEnumTricks
+//
+//  Created by Ellen Shapiro on 5/15/18.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+import UIKit
+
+public enum Asset: String {
+  case cinnamon_rolls
+  case innocent
+  case no
+  case snack
+  case window
+
+  var image: UIImage {
+    return UIImage(named: self.rawValue)!
+  }
+  
+  // A simulation of `CaseIterable`, coming in Swift 4.2
+  static var allCases: [Asset] {
+    return [
+      .cinnamon_rolls,
+      .innocent,
+      .no,
+      .snack,
+      .window,
+    ]
+  }
+}
+
+
