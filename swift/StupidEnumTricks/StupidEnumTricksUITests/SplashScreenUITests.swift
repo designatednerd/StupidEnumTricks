f1d08ccd1dc8658c9b4872df9e5cdcd02283ab92
Ellen Shapiro <designatednerd@gmail.com>
2018-05-16 00:39:41 +0200

    stub out UI tests

---
+++ swift/StupidEnumTricks/StupidEnumTricksUITests/SplashScreenUITests.swift
@@ -0,0 +1,21 @@
+//
+//  SplashScreenUITests.swift
+//  StupidEnumTricksUITests
+//
+//  Created by Ellen Shapiro on 5/15/18.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+import XCTest
+import StupidEnumTricks
+
+class SplashScreenUITests: XCTestCase {
+  
+  func testTappingSignInGoesToCorrectPlace() {
+    
+  }
+  
+  func testTappingSignUpGoesToCorrectPlace() {
+    
+  }
+}
